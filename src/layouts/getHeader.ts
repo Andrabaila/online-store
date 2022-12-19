47d3098072405d56690d4a74f6cb92cb24cce2dd
Katsiaryna-Andrabaila <106549356+Katsiaryna-Andrabaila@users.noreply.github.com>
2022-12-19 09:31:56 +0300

    Header layout (#2)
    * feat: add basic header structure
    
    * feat: add header layout
    
    * fix: remove space between $ and 0
    
    * fix: rewrite styles for products-in-cart
    
    * fix: change color format on rgba
    
    * fix: rewrite styles according to correct format

---
+++ src/layouts/getHeader.ts
@@ -0,0 +1,20 @@
+export function getHeader() {
+    const header: HTMLElement = document.body.appendChild(document.createElement('header'));
+    header.classList.add('header');
+
+    const logo: HTMLAnchorElement = header.appendChild(document.createElement('a'));
+    logo.classList.add('logo');
+    logo.href = '../index.html';
+    logo.textContent = 'Online Store';
+
+    const cartTotal: HTMLDivElement = header.appendChild(document.createElement('div'));
+    cartTotal.classList.add('sum-total');
+    cartTotal.textContent = 'Cart total: $0';
+
+    const cart: HTMLDivElement = header.appendChild(document.createElement('div'));
+    cart.classList.add('cart');
+
+    const productsInCart: HTMLDivElement = cart.appendChild(document.createElement('div'));
+    productsInCart.classList.add('products-in-cart');
+    productsInCart.textContent = '0';
+}
---
+++ src/pages/getMainPage.ts
@@ -0,0 +1,5 @@
+import { getHeader } from '../layouts/getHeader';
+
+export function getMainPage() {
+    getHeader();
+}
