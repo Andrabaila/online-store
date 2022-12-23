0e79246ce1312ad32eb8b7907385e9f435ea6cbf
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-12-23 15:27:33 +0300

    feat: add styles for product-list layout

--- src/layouts/getProductsList.ts
+++ src/layouts/getProductsList.ts
@@ -1,3 +1,5 @@
+import { showProduct } from './showProduct';
+
 export async function getProductsList(parent: HTMLDivElement) {
     const products = '../assets/products.json';
     const response = await fetch(products);
@@ -7,14 +9,15 @@ export async function getProductsList(parent: HTMLDivElement) {
     productList.classList.add('main__item_product-list');
 
     for (let i = 0; i < data['products'].length; i += 1) {
-        productList.appendChild(document.createElement('div')).classList.add('main__item_product');
+        const productItem = productList.appendChild(document.createElement('div'));
+        productItem.classList.add('main__item_product');
+        productItem.addEventListener('click', showProduct);
     }
 
-    const childElements = Object.values(productList.childNodes) as HTMLDivElement[];
+    const childElements = <HTMLDivElement[]>Object.values(productList.childNodes);
     for (let i = 0; i < childElements.length; i += 1) {
-        childElements[i].style.background = `url(${data['products'][i].thumbnail}) center center no-repeat`;
+        childElements[i].style.background = `url(${data['products'][i].thumbnail}) right center no-repeat #ffffff`;
         childElements[i].style.backgroundSize = 'contain';
-        childElements[i].style.backgroundColor = '#ffffff';
 
         const name = childElements[i].appendChild(document.createElement('p'));
         name.classList.add('main__item_product-name');
@@ -24,24 +27,24 @@ export async function getProductsList(parent: HTMLDivElement) {
         description.classList.add('main__item_product-description');
         description.textContent = `${data['products'][i].description}`;
 
+        const discount = childElements[i].appendChild(document.createElement('p'));
+        discount.innerHTML = `Discount: <br><b>${data['products'][i].discountPercentage}</b>`;
+
+        const rating = childElements[i].appendChild(document.createElement('p'));
+        rating.innerHTML = `Rating: <br><b>${data['products'][i].rating}</b>`;
+
+        const stock = childElements[i].appendChild(document.createElement('p'));
+        stock.innerHTML = `In stock: <br><b>${data['products'][i].stock}</b>`;
+
+        const brand = childElements[i].appendChild(document.createElement('p'));
+        brand.innerHTML = `Brand: <br><b>${data['products'][i].brand}</b>`;
+
+        const category = childElements[i].appendChild(document.createElement('p'));
+        category.innerHTML = `Category: <br><b>${data['products'][i].category}</b>`;
+
         const price = childElements[i].appendChild(document.createElement('p'));
         price.classList.add('main__item_product-price');
         price.textContent = `$${data['products'][i].price}`;
-
-        const discount = childElements[i].appendChild(document.createElement('p'));
-        discount.textContent = `Discount: ${data['products'][i].discountPercentage}`;
-
-        const rating = childElements[i].appendChild(document.createElement('p'));
-        rating.textContent = `Rating: ${data['products'][i].rating}`;
-
-        const stock = childElements[i].appendChild(document.createElement('p'));
-        stock.textContent = `In stock: ${data['products'][i].stock}`;
-
-        const brand = childElements[i].appendChild(document.createElement('p'));
-        brand.textContent = `Brand: ${data['products'][i].brand}`;
-
-        const category = childElements[i].appendChild(document.createElement('p'));
-        category.textContent = `Category: ${data['products'][i].category}`;
 
         const btn = childElements[i].appendChild(document.createElement('button'));
         btn.classList.add('main__item_product-add-button');
--- src/layouts/getProductsOptions.ts
+++ src/layouts/getProductsOptions.ts
@@ -1,4 +1,5 @@
-import { SORT_OPTIONS, SMALL_VIEW_CELLS, LARGE_VIEW_CELLS } from '../constants/constants';
+import { OPTIONS_TEXT, SORT_OPTIONS, SMALL_VIEW_CELLS, LARGE_VIEW_CELLS } from '../constants/constants';
+import { changeView } from './changeView';
 
 export function getProductsOptions(parent: HTMLDivElement) {
     const options = parent.appendChild(document.createElement('div'));
@@ -6,35 +7,36 @@ export function getProductsOptions(parent: HTMLDivElement) {
 
     const sortField: HTMLSelectElement = options.appendChild(document.createElement('select'));
     sortField.classList.add('main__item_options-sort');
-    const selectHeader: HTMLOptionElement = sortField.appendChild(document.createElement('option'));
-    selectHeader.textContent = 'Sort by:';
-    selectHeader.setAttribute('disabled', 'disabled');
 
     for (let i = 0; i < SORT_OPTIONS.length; i += 1) {
         sortField.appendChild(document.createElement('option')).textContent = SORT_OPTIONS[i];
     }
+    (<HTMLOptionElement>sortField.firstChild).setAttribute('disabled', 'disabled');
 
     const found: HTMLParagraphElement = options.appendChild(document.createElement('p'));
     found.classList.add('main__item_options-found');
-    found.textContent = 'Found: 30';
+    found.textContent = OPTIONS_TEXT[0];
 
     const searchField: HTMLInputElement = options.appendChild(document.createElement('input'));
     searchField.classList.add('main__item_options-search');
     searchField.type = 'search';
-    searchField.placeholder = 'Search product';
+    searchField.placeholder = OPTIONS_TEXT[1];
 
     const views: HTMLDivElement = options.appendChild(document.createElement('div'));
     views.classList.add('main__item_options-views');
 
     const smallView: HTMLDivElement = views.appendChild(document.createElement('div'));
     smallView.classList.add('main__item_options-small');
+    smallView.classList.add('active-view');
     for (let i = 0; i < SMALL_VIEW_CELLS; i += 1) {
         smallView.appendChild(document.createElement('div')).classList.add('main__item_options-small-cell');
     }
+    smallView.addEventListener('click', changeView);
 
     const largeView: HTMLDivElement = views.appendChild(document.createElement('div'));
     largeView.classList.add('main__item_options-large');
     for (let i = 0; i < LARGE_VIEW_CELLS; i += 1) {
         largeView.appendChild(document.createElement('div')).classList.add('main__item_options-large-cell');
     }
+    largeView.addEventListener('click', changeView);
 }
