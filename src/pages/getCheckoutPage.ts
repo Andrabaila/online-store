0cbbe72a8fbd66e7f20c9fe790e73605b3fe7405
Yury Andrabaila <Androbailo@gmail.com>
2022-12-28 00:55:52 +0200

    refactor: add loop and speaking names

--- src/pages/getCheckoutPage.ts
+++ src/pages/getCheckoutPage.ts
@@ -13,49 +13,20 @@ export function getCheckoutPage() {
     xButton.addEventListener('click', toggleCheckout);
     getHtmlElement({ parent: '.checkout', tag: 'h3', style: ['checkout__title'], content: UI.checkoutTitle1 });
 
-    getHtmlElement({ parent: '.checkout', style: ['checkout__input-container'] });
-    const input1 = <HTMLInputElement>getHtmlElement({
-        parent: '.checkout__input-container',
-        tag: 'input',
-        style: ['input', 'input_checkout'],
-        name: 'name',
-    });
-    input1.placeholder = UI.checkoutPlaceholder1;
-    input1.addEventListener('keyup', validateInput);
-    getHtmlElement({ parent: '.checkout__input-container', style: ['checkout__message'] });
+    const inputsToDo = ['name', 'phone', 'address', 'email'];
 
-    getHtmlElement({ parent: '.checkout', style: ['checkout__input-container'] });
-    const input2 = <HTMLInputElement>getHtmlElement({
-        parent: '.checkout__input-container',
-        tag: 'input',
-        style: ['input', 'input_checkout'],
-        name: 'phone',
-    });
-    input2.placeholder = UI.checkoutPlaceholder2;
-    input2.addEventListener('keyup', validateInput);
-    getHtmlElement({ parent: '.checkout__input-container', style: ['checkout__message'] });
-
-    getHtmlElement({ parent: '.checkout', style: ['checkout__input-container'] });
-    const input3 = <HTMLInputElement>getHtmlElement({
-        parent: '.checkout__input-container',
-        tag: 'input',
-        style: ['input', 'input_checkout'],
-        name: 'address',
-    });
-    input3.placeholder = UI.checkoutPlaceholder3;
-    input3.addEventListener('keyup', validateInput);
-    getHtmlElement({ parent: '.checkout__input-container', style: ['checkout__message'] });
-
-    getHtmlElement({ parent: '.checkout', style: ['checkout__input-container'] });
-    const input4 = <HTMLInputElement>getHtmlElement({
-        parent: '.checkout__input-container',
-        tag: 'input',
-        style: ['input', 'input_checkout'],
-        name: 'email',
-    });
-    input4.placeholder = UI.checkoutPlaceholder4;
-    input4.addEventListener('keyup', validateInput);
-    getHtmlElement({ parent: '.checkout__input-container', style: ['checkout__message'] });
+    for (let i = 0; i < inputsToDo.length; i++) {
+        getHtmlElement({ parent: '.checkout', style: ['checkout__input-container'] });
+        const input = <HTMLInputElement>getHtmlElement({
+            parent: '.checkout__input-container',
+            tag: 'input',
+            style: ['input', 'input_checkout'],
+            name: inputsToDo[i],
+        });
+        input.placeholder = UI[`checkoutPlaceholder${i + 1}`];
+        input.addEventListener('keyup', validateInput);
+        getHtmlElement({ parent: '.checkout__input-container', style: ['checkout__message'] });
+    }
 
     getHtmlElement({ parent: '.checkout', tag: 'h3', style: ['checkout__title'], content: UI.checkoutTitle2 });
 
@@ -63,15 +34,15 @@ export function getCheckoutPage() {
     getHtmlElement({ parent: '.card-data', style: ['checkout__input-container_wide'] });
     getHtmlElement({ parent: '.checkout__input-container_wide', style: ['checkout__card-logo'] });
 
-    const input5 = <HTMLInputElement>getHtmlElement({
+    const inputCardNumber = <HTMLInputElement>getHtmlElement({
         parent: '.checkout__input-container_wide',
         tag: 'input',
         style: ['input', 'input_checkout'],
         name: 'cardNumber',
     });
-    input5.placeholder = UI.checkoutPlaceholder5;
-    input5.addEventListener('keyup', validateInput);
-    input5.addEventListener('keyup', showCardLogo);
+    inputCardNumber.placeholder = UI.checkoutPlaceholder5;
+    inputCardNumber.addEventListener('keyup', validateInput);
+    inputCardNumber.addEventListener('keyup', showCardLogo);
     getHtmlElement({ parent: '.checkout__input-container_wide', style: ['checkout__message'] });
 
     getHtmlElement({ parent: '.card-data', style: ['card-data__container'] });
@@ -82,15 +53,15 @@ export function getCheckoutPage() {
         style: ['card-data__label'],
         content: UI.checkoutLabel1,
     });
-    const input6 = <HTMLInputElement>getHtmlElement({
+    const inputCardDate = <HTMLInputElement>getHtmlElement({
         parent: '.card-data__label',
         tag: 'input',
         style: ['input', 'card-data__input'],
         name: 'cardDate',
     });
-    input6.placeholder = UI.checkoutPlaceholder6;
-    input6.addEventListener('keyup', validateInput);
-    input6.addEventListener('keyup', autoFill);
+    inputCardDate.placeholder = UI.checkoutPlaceholder6;
+    inputCardDate.addEventListener('keyup', validateInput);
+    inputCardDate.addEventListener('keyup', autoFill);
     getHtmlElement({ parent: '.card-data__label', style: ['checkout__message'] });
 
     getHtmlElement({ parent: '.card-data__container', style: ['checkout__input-container'] });
@@ -100,14 +71,14 @@ export function getCheckoutPage() {
         style: ['card-data__label'],
         content: UI.checkoutLabel2,
     });
-    const input7 = <HTMLInputElement>getHtmlElement({
+    const inputCardCode = <HTMLInputElement>getHtmlElement({
         parent: '.card-data__label',
         tag: 'input',
         style: ['input', 'card-data__input'],
         name: 'cardCode',
     });
-    input7.placeholder = UI.checkoutPlaceholder7;
-    input7.addEventListener('keyup', validateInput);
+    inputCardCode.placeholder = UI.checkoutPlaceholder7;
+    inputCardCode.addEventListener('keyup', validateInput);
     getHtmlElement({ parent: '.card-data__label', style: ['checkout__message'] });
 
     getHtmlElement({
