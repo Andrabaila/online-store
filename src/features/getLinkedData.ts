02cda982ba1100bee436ae4d12461177d77e118b
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-12-28 17:41:49 +0300

    refactor: add const for id field in getLinkedData

--- src/features/getLinkedData.ts
+++ src/features/getLinkedData.ts
@@ -1,11 +1,11 @@
 import { Product } from '../data/types';
-import { ID_INDEX } from '../constants/constants';
 
 export function getLinkedData(data: Product[]): Product[] {
     const productList = document.querySelectorAll('.main__item_product');
     const dataList: Product[] = [];
     for (let i = 0; i < productList.length; i++) {
-        const id = productList[i].childNodes[ID_INDEX].textContent;
+        const idField = productList[i].childNodes[0];
+        const id = idField.textContent;
         dataList.push(data[Number(id) - 1]);
     }
     return dataList;
