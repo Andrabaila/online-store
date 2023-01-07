d25f633642297f0099870e4be24054717e261588
Yury Andrabaila <Androbailo@gmail.com>
2023-01-07 22:48:53 +0200

    fix: 404 page

--- src/features/routePages.ts
+++ src/features/routePages.ts
@@ -9,7 +9,7 @@ export function routePages() {
     const page = hash.split('-')[0];
     const id = hash.split('-')[1];
 
-    if (pathname !== '/' && !hash) {
+    if (!(pathname === '/' && hash.slice(0, 1) === '#')) {
         get404Page();
         return;
     }
