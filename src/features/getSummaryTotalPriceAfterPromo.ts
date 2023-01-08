b80b6ec455257b572c07ddce44e8f64222c60b0b
katsiaryna-andrabaila <androbailoe@gmail.com>
2023-01-08 11:23:17 +0300

    refactor: rewrite price after promo with ternary operator

--- src/features/getSummaryTotalPriceAfterPromo.ts
+++ src/features/getSummaryTotalPriceAfterPromo.ts
@@ -5,11 +5,5 @@ export function getSummaryTotalPriceAfterPromo() {
     const price = Number(getSummaryTotalPriceBeforePromo());
     const discount = PROMO_CODES.reduce((acc, el) => acc + el, 0);
     const multiplier = (100 - discount) / 100;
-    let resultPrice: number;
-    if (PROMO_CODES.length) {
-        resultPrice = price * multiplier;
-    } else {
-        resultPrice = price;
-    }
-    return resultPrice.toFixed(2);
+    return PROMO_CODES.length ? (price * multiplier).toFixed(2) : price.toFixed(2);
 }
