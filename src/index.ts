0598c54fcaac555e7fa3cff6fde283e237ee275f
katsiaryna-andrabaila <androbailoe@gmail.com>
2023-01-08 11:24:12 +0300

    fix: remove comments

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import { getLocalStorage } from './features/getLocalStorage';
 window.addEventListener('beforeunload', setLocalStorage);
 
 window.addEventListener('hashchange', routePages);
-//localStorage.clear();
-//setLocalStorage();
+
 getLocalStorage().then(() => {
     routePages();
 });
